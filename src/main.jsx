af36813f5f1d86d5e546161742cd1d2c3c236105
alytech8 <alytech8@gmail.com>
2026-05-08 16:15:31 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import WhatsAppHelper from './WhatsAppHelper'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <WhatsAppHelper />
+    <App />
   </React.StrictMode>
 )
